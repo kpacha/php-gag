26cd81fb5eb9f0561bdd66cc0727216ded7e85a0
Kpacha <kpacha666@gmail.com>
2013-07-08 01:13:04 +0200

    extending the examples

--- examples/aop.php
+++ examples/aop.php
@@ -15,6 +15,9 @@ use Kpacha\PhpGag\Aspects;
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 AnnotationRegistry::registerFile('NonGAGAnnotation.php');
 
+//Get Doctrine Reader
+$reader = new AnnotationReader();
+
 // instantiate the Symfony2 DIC
 $container = new ContainerBuilder();
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__));
@@ -29,7 +32,7 @@ $refClass = new ReflectionClass('\Example\Dummy');
 $filename = $proxyDir . str_replace('\\', '-', $refClass->name) . '.php';
 
 $pg = new ProxyGenerator($refClass, array(
-            new AspectGenerator(new AnnotationReader(), '\PUGX\AOP\Aspect\BaseAnnotation'),
+            new AspectGenerator($reader, '\PUGX\AOP\Aspect\BaseAnnotation'),
         ));
 $pg->writeClass($filename);
 require $filename;
@@ -43,6 +46,19 @@ $cantTouchThisAspect = new Aspects\CantTouchThis\CantTouchThis($container);
 $imaLetYouFinishButAspect = new Aspects\ImaLetYouFinishBut\ImaLetYouFinishBut($container);
 
 $proxy = new $proxyClass(1, 2, $loggableAspect, $noopAspect, $rouletteAspect, $cantTouchThisAspect, $imaLetYouFinishButAspect);
+
+// let's filter our proxy first!!
+use DMS\Filter\Mapping\ClassMetadataFactory,
+    DMS\Filter\Filter,
+    Kpacha\PhpGag\Mapping\Loader\AnnotationLoader;
+
+// Get a Filter
+$filter = new Filter(new ClassMetadataFactory(new AnnotationLoader($reader)));
+
+$filter->filterEntity($proxy);
+echo $proxy->getA();
+
+// now, let's call some enhaced metods!
 echo $proxy->doSomething(5);
 $proxy->replay("supu, tupu, lapu\n");
 echo $proxy->tryToTouch();
