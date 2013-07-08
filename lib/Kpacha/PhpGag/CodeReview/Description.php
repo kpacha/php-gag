49fbb852b188bc6f9272f38711f81dbfca1f911d
Kpacha <kpacha666@gmail.com>
2013-07-08 22:39:21 +0200

    tracking some annotation data in a single object

---
+++ lib/Kpacha/PhpGag/CodeReview/Description.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kpacha\PhpGag\CodeReview;
+
+class Description
+{
+
+    protected $annotation;
+    protected $line;
+
+    public function __construct($line, $annotation)
+    {
+        $this->line = $line;
+        $this->annotation = $annotation;
+    }
+
+    public function getAnnotation()
+    {
+        return $this->annotation;
+    }
+
+    public function setAnnotation($annotation)
+    {
+        $this->annotation = $annotation;
+    }
+
+    public function getLine()
+    {
+        return $this->line;
+    }
+
+    public function setLine($line)
+    {
+        $this->line = (int) $line;
+    }
+
+}
