41e16b2abe5aacff46ed60e333a8a1b3ba9bd917
Kpacha <kpacha666@gmail.com>
2013-07-08 22:47:13 +0200

    extending the examples

--- examples/codeReview.php
+++ examples/codeReview.php
@@ -3,10 +3,15 @@
 require_once '../vendor/autoload.php';
 
 use Kpacha\PhpGag\CodeReview\Analyzer,
-    Kpacha\PhpGag\CodeReview\Inspector,
+    Kpacha\PhpGag\CodeReview\OptimizedInspector,
+    Kpacha\PhpGag\CodeReview\Reporter,
     Doctrine\Common\Annotations\AnnotationReader,
-    Kpacha\PhpGag\Mapping\Loader\AnnotationLoader;
+    Kpacha\PhpGag\Mapping\Loader\AnnotationLoader,
+    Kpacha\PhpGag\CodeReview\Browser\CLIController;
 use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$logPath = __DIR__ . DIRECTORY_SEPARATOR . 'log';
+$sourcePath = realpath(__DIR__ . '/../tests/Kpacha/Tests/PhpGag/Aspects/Mocks');
 
 //Get Doctrine Reader
 $reader = new AnnotationReader();
@@ -16,10 +21,42 @@ AnnotationRegistry::registerFile('NonGAGAnnotation.php');
 new AnnotationLoader($reader);
 
 //Get the analyser
-$analyzer = new Analyzer(new Inspector($reader));
+$analyzer = new Analyzer(new OptimizedInspector($reader), new Reporter($logPath));
 
-//Analyze you file
-$analysis = $analyzer->addFile('User.php')->addFile('Dummy.php')->compile()->getAnnotations();
+//Analyze your files
+$analyzer
+        ->addFolder($sourcePath)
+        ->compile()
+;
 
-var_dump($analysis['User.php']['Example\User']);
-var_dump($analysis['Dummy.php']['Example\Dummy']);
+if (strpos('@php_dir@', '@php_dir') === 0) {
+    require_once dirname(__FILE__) . '/../vendor/mayflower/php-codebrowser/src/CLIController.php';
+} else {
+    require_once '@php_dir@/PHP_CodeBrowser/CLIController.php';
+}
+$controller = new CLIController(
+                $logPath,
+                array($sourcePath),
+                __DIR__ . '/output',
+                array(),
+                array(),
+                array(),
+                new CbIOHelper(),
+                Log::factory('console', '', 'PHPCB'),
+                array('php'),
+                false
+);
+
+$controller->addErrorPlugins(CLIController::getAvailablePlugins());
+
+try {
+    $controller->run();
+} catch (Exception $e) {
+    error_log(
+            <<<HERE
+[Error] {$e->getMessage()}
+
+{$e->getTraceAsString()}
+HERE
+    );
+}
