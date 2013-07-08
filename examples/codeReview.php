1c4ab51f910b513443a652827ac327e5f421406a
Kpacha <kpacha666@gmail.com>
2013-07-09 00:51:33 +0200

    refactoring the code review example

--- examples/codeReview.php
+++ examples/codeReview.php
@@ -2,18 +2,15 @@
 
 require_once '../vendor/autoload.php';
 
-use Kpacha\PhpGag\CodeReview\CodeReview,
-    Doctrine\Common\Annotations\AnnotationReader,
-    Kpacha\PhpGag\Mapping\Loader\AnnotationLoader;
+use Kpacha\PhpGag\CodeReview\CodeReview;
+use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $logPath = __DIR__ . DIRECTORY_SEPARATOR . 'log';
 $sourcePath = realpath(__DIR__ . '/../tests/Kpacha/Tests/PhpGag/Aspects/Mocks');
 
-//Get Doctrine Reader
-$reader = new AnnotationReader();
+// Register foreign annotations
 AnnotationRegistry::registerFile('NonGAGAnnotation.php');
 
-
-$codeReview = new CodeReview($reader, $logPath, $sourcePath, __DIR__ . '/output');
+$codeReview = new CodeReview(new AnnotationReader(), $logPath, $sourcePath, __DIR__ . '/output');
 $codeReview->analyze()->report();
